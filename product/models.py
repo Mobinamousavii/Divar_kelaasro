0acc64817688791e3e80fac8f6adf76c8bc0c95e
Mobinamousavii <mobinamsv6@gmail.com>
2025-04-16 13:17:30 +0330

    models for user and product added

--- product/models.py
+++ product/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from user.models import User
 
-# Create your models here.
+class Product(models.Model):
+    title =models.CharField(max_length=100)
+    price = models.IntegerField()
+    date_time = models.DateTimeField(auto_now_add=True)
+    info = models.CharField(max_length=100, null=True, blank=True)
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    class ProductType(models.TextChoices):
+        REAL_ESTATAE = 'REAL_ESTATE'
+        VEHICLES = 'VEHICLES'
+        DIGITAL_GOODS = 'DIGITAL_GOODS'
+        HOME_KITCHEN = 'HOME_KITCHEN'
+        SERVICES = 'SERVICES'
+        PERSONAL_ITEM = 'PERSONAL_ITEMS'
+        ENTERTAINMENT_LEISURE = 'ENTERTAINMENT_LEISURE'
+        COMMUNITY = 'COMMUNITY'
+        INDUSTRIAL_EQUIPMENT = 'INDUSTRIAL EQUIPMENT'
+        JOBS_EMPLOYMENT = 'JOBS & EMPLOYMENT'
+
+    type = models.CharField(  max_length=30, choices=ProductType.choices, null=False, blank=False)
+    
+
+
+
+
+
+
+
+
+
+
+
--- user/models.py
+++ user/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=50)
+    password = models.CharField(max_length=50)
+    phone = models.CharField(max_length=50)
+    email = models.EmailField(null=True, blank=True)
+    national_id = models.CharField(max_length=11, unique=True)
+
+    def __str__(self):
+        return self.username
